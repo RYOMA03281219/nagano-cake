303a850524522bec0b29015ed2163c85714567d2
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-13 13:41:00 +0000

    [Add]データ型の編集

--- db/migrate/20230413110420_create_orders.rb
+++ db/migrate/20230413110420_create_orders.rb
@@ -1,6 +1,15 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :customer_id, null: false
+      t.string :postal_code, null: false
+      t.string :address, null: false
+      t.string :name, null: false
+      t.string :postage, null: false
+      t.string :billing_amount, null: false
+      t.integer :payment_method, null:false
+      t.boolean :order_status, null: false, default: true
+
 
       t.timestamps
     end
