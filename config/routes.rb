d31f931a004072173838d892ba5aac864ef211ab
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-14 14:32:50 +0000

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,63 +1,16 @@
 Rails.application.routes.draw do
-  # 会員側のルーティング設定→public
-  get '/'=> 'public/homes#top', as: "root"
-  devise_for :customers
-  
-  namespace :public do
-    get 'cart_items/index'
-    get 'cart_items/update'
-    get 'cart_items/destroy'
-    get 'cart_items/destroy_all'
-    get 'cart_items/create'
-  end
-  namespace :public do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  namespace :public do
-    get 'registrations/new'
-    get 'registrations/create'
-  end
-  namespace :public do
-    get 'items/index'
-    get 'items/show'
-  end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
- 
+   # 会員側のルーティング設定
+  devise_for :public
+  get "/home/about" => "homes#about"
+  get 'items' => 'public/items#index'
 
-  # 管理者側のルーティング設定→admin
-  divise_for :admins
+
+  # 管理者側のルーティング設定
+  devise_for :admin
   namespace :admin do
+  root to: 'homes#top'
+  resources :customers, only: [:edit, :index, :show, :update]
+
     get 'items' => 'admin/items#index'
   end
-  root to: 'homes#top'
-
-  namespace :admin do
-    get 'orders/show'
-  end
-  namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
-  end
-  namespace :admin do
-    get 'items/index'
-    get 'items/new'
-    get 'items/create'
-    get 'items/show'
-    get 'items/edit'
-    get 'items/update'
-  end
-  namespace :admin do
-    get 'sessions/new'
-    get 'sessions/create'
-    get 'sessions/destroy'
-  end
-  
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
