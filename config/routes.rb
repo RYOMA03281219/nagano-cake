77987522ce248b29d805c70c7e0920f479866d93
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-15 15:59:40 +0000

    [Add]コードの修正、adminのログインページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-   # 会員側のルーティング設定
-  devise_for :public
-  get "/home/about" => "homes#about"
-  get 'items' => 'public/items#index'
-
-
   # 管理者側のルーティング設定
-  devise_for :admin
+  devise_for :admins
   namespace :admin do
-  root to: 'homes#top'
+  root to: 'sessions#new'
+
   resources :customers, only: [:edit, :index, :show, :update]
 
     get 'items' => 'admin/items#index'
