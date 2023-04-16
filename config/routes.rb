3b89e762f33ed68d1d26fb6812ac0262f79d5aff
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-16 15:39:54 +0000

    [Fix]顧客用と管理者用のルート修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  # 管理者側のルーティング設定
-  devise_for :admins
-  namespace :admin do
-  root to: 'sessions#new'
+# 顧客用
+# URL /customers/sign_in ...
+devise_for :customers,skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
 
+
+
+# 管理者側のルーティング設定
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  namespace :admin do
+  get '' => 'homes#top'
   resources :customers, only: [:edit, :index, :show, :update]
 
-    get 'items' => 'admin/items#index'
+
   end
 end
