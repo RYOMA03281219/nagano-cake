3fc9abc7c8630c8c704334875d3a2b9b9d63f85c
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-19 13:24:14 +0000

    [Fix]itemのresources作成、一部ルーティング削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
 # 顧客用
+devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+  namespace :admin do
+  resources :items, only: [:index, :new, :create, :show, :edit, :update]
+  resources :customers, only: [:edit, :index, :show, :update]
+    get '/' => 'homes#top'
+  end
+  
 # URL /customers/sign_in ...
 devise_for :customers,skip: [:passwords], controllers: {
   registrations: "public/registrations",
@@ -11,21 +20,13 @@ devise_for :customers,skip: [:passwords], controllers: {
 # 管理者側のルーティング設定
 # 管理者用
 # URL /admin/sign_in ...
-devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-}
-  namespace :admin do
-    devise_for :admins, skip: :all
-    devise_scope :admins do
-    get '/sign_in' => 'sessions#new'
-    post '/sign_in' => 'sessions#create'
-    delete '/sign_out' => 'sessions#destroy'
-    get '/' => 'homes#top'
-  end
 
 
-  resources :customers, only: [:edit, :index, :show, :update]
 
 
-  end
 end
+  #   # devise_for :admins, skip: :all
+  #   # devise_scope :admins do
+  #   # get '/sign_in' => 'sessions#new'
+  #   # post '/sign_in' => 'sessions#create'
+  #   # delete '/sign_out' => 'sessions#destroy'
