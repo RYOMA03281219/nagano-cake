8b62fdfd9e9d000058ff2c09c7d5c0a428fb2862
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-19 13:23:38 +0000

    [Add]管理者ログインPW/nameの指定

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+  Admin.create!(
+    email: 'ryu@ntt.com',
+    password: 'ryu1010'
+    )
