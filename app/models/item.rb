c0c4789a309b9735465d2a6152c6337ef97a48a4
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-21 16:31:28 +0000

    [Add]各validatesの記述を追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  validates checked, inclusion: [true, false]
+  validates :image, presence: {message: '選択されていません'}
+  validates :name, presence: true
+  validates :introduction, presence: true
+  belongs_to :genre
+  validates :price, numericality: {only_integer: true}
+  validates :item, inclusion: {in:[true,false]}
 end
