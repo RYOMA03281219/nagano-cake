f137bf7d15d0f3b387b2d3c0450b49ac4d179a61
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-23 10:46:37 +0000

    [Add]Genreのモデルを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-  Admin.create!(
-    email: 'ryu@ntt.com',
-    password: 'ryu1010'
-    )
+Admin.create!(
+  email: 'ryu@ntt.com',
+  password: 'ryu1010'
+  )
+  
+Genre.create!(
+  name: 'ケーキ'
+  )
