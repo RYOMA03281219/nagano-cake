aeec0dda4d252268bf94a2204d7fa08c9f67a4e9
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-23 10:47:24 +0000

    [Add]has_one_attachedと幅とサイズ、デフォルト画像の表示コマンドを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,19 @@
 class Item < ApplicationRecord
-  validates checked, inclusion: [true, false]
   validates :image, presence: {message: '選択されていません'}
   validates :name, presence: true
   validates :introduction, presence: true
+  validates :price, numericality: {only_integer: true,}
+  # validates :item, inclusion: {in:[true,false]}
+
   belongs_to :genre
-  validates :price, numericality: {only_integer: true}
-  validates :item, inclusion: {in:[true,false]}
+
+  has_one_attached :image
+
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('./app/assets/images/default-image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [width, height]).processed
+  end
 end
