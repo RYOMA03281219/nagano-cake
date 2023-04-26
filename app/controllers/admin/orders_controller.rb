94d60432e64cfe7a0f01d2889d560b4531c35d62
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-04-26 13:36:12 +0000

    [Add]注文履歴詳細画面の作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,13 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_detail
+    @total = @order_details.inject(0) { |sum, order| sum + order.sum_of_price }
+  end
+
+  private
+
+  def order_details_params
+    params.require(:order_details).permit(:purchase_price, :amount, :production_status)
   end
 end
