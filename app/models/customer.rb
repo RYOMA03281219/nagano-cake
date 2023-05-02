19d9887fb03c0fa89fcf7bc2ebf9ed585e8db13a
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-02 13:08:30 +0000

    [Add]validatesの追加

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,16 @@
 class Customer < ApplicationRecord
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :postal_code, presence: true
+  validates :address, presence: true
+  validates :telephone_number, presence: true
+  validates :password,  {presence: true, length: {minimum: 6}}
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
 end
