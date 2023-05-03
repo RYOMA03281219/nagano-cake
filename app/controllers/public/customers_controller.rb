d00c0d1ea015411df3f51cec32dde5b32eeb2ecf
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-03 08:49:10 +0000

    [Add]各アクションの追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,17 @@
 class Public::CustomersController < ApplicationController
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def cancellation
+  end
+
+  def withdrawal
+  end
+
 end
