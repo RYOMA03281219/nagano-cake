483f08eaf59197aad145c3101e772c853995cafc
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-03 08:30:29 +0000

    [Add]create,destroy_allアクションの追加

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,6 @@
 class Public::CartItemsController < ApplicationController
   def index
+
   end
 
   def update
@@ -9,8 +10,28 @@ class Public::CartItemsController < ApplicationController
   end
 
   def destroy_all
+    current_custmer.curtitems.destroy_all
+    redirect_to cart_items_path
   end
 
   def create
+    #binding.pry
+    @cart_items = CartItem.find(params[:id])
+    
+    if @cart_items.save!
+      redirect_to cart_items_path(@cart_items.id)
+    else
+      render "new"
+    end
+      2. カート内の個数をフォームから送られた個数分追加する
+    存在しなかった場合
+      カートモデルにレコードを新規作成する
+    
+  end
+
+  private
+
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :amount)
   end
 end
