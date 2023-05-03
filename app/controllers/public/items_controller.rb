8efc6723e49db606dfccd88905dcf88a7694c03e
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-03 08:48:49 +0000

    [Add]index,showアクションの追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,7 +1,13 @@
 class Public::ItemsController < ApplicationController
   def index
+  # ページネーションの機能
+    @items = Item.all.page(params[:page]).per(10)
+    @item_count = Item.all.count
+
   end
 
   def show
+    @customer = current_customer
+    @item = Item.find(params[:id])
   end
 end
