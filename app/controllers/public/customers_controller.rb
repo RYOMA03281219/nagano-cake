a2d43850d125c84b4fb2770d4705c037c6666674
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-04 18:19:33 +0000

    [Add]showアクションの追加

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,17 +1,34 @@
 class Public::CustomersController < ApplicationController
   def show
+    @customer = Customer.find(current_customer.id)
   end
 
   def edit
+    @customer = Customer.find(current_customer.id)
   end
 
   def update
+    @customer = Customer.find(current_customer.id)
+    if @customer.update(customer_params)
+      redirect_to customers_my_page_path
+    else
+      render "edit"
+    end
   end
 
   def cancellation
+    @customer = Customer.find(current_customer.id)
   end
 
   def withdrawal
+    @customer = Customer.find(current_customer.id)
+    @customer.destroy
+    redirect_to customers_cancellation_path
+  end
+
+ private
+  def customer_params
+     params.require(:customer).permit(:email, :last_name, :first_name, :last_name_kana, :first_name_kana,  :postal_code, :address, :telephone_number)
   end
 
 end
