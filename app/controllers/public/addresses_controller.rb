02d3556a38f046e8b2bfba68828ad2367d38aa2a
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-04 19:54:57 +0000

    [Add]各アクションの追加

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,7 +1,41 @@
 class Public::AddressesController < ApplicationController
   def index
+    @address = Address.all.page(params[:page]).per(10)
+  end
+
+  def create
+    @address = Address.new(address_params)
+    @customer = Customer.find(current_customer.id)
+    if @address.save
+      redirect_to addresses_path
+    else
+      render "index"
+    end
   end
 
   def edit
+    @customer = current_customer
+    @address = Address.find(params[:id])
+  end
+
+  def update
+    @address =Address.find(params[:id])
+    if @address.update(address_params)
+      redirect_to addresses_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @address = Address.find(params[:id])
+    @customer.destroy
+    redirect_to addresses_path
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(:name, :postal_code, :address)
   end
 end
