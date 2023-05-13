4b58441dff90e5329a8377f7939c63bad0fa0c69
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-13 19:44:07 +0000

    [Add]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,6 +1,7 @@
 class Public::ItemsController < ApplicationController
   def index
   # ページネーションの機能
+    @customer = current_customer
     @items = Item.all.page(params[:page]).per(10)
     @item_count = Item.all.count
 
@@ -11,4 +12,5 @@ class Public::ItemsController < ApplicationController
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
   end
+  
 end
