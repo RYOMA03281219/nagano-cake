e0088e55e716236c3ed5744b755dc12e2aacad5a
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-13 19:43:09 +0000

    [Fix]showアクションのtotalの変数を修正

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -2,7 +2,7 @@ class Admin::OrdersController < ApplicationController
   def show
     @order = Order.find(params[:id])
     @order_details = @order.order_details
-    @total = @order_details.inject(0) { |sum, order| sum + order.purchase_price }
+    @total = (@order_details.inject(0) { |sum, cart_items| sum + cart_items.subtotal })
   end
 
   private
