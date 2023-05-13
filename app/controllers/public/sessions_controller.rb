9b50b64ba967d58cd30b936162a4836dfcb2ec7e
EC2 Default User <ec2-user@ip-172-31-26-57.ap-northeast-1.compute.internal>
2023-05-13 15:41:17 +0000

    [Add]フラッシュメッセージ追加

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -3,7 +3,8 @@
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   def after_sign_in_path_for(resource)
-    new_customer_session_path
+    flash[:notice] = "ようこそ" + current_customer.last_name + "さん！"
+    customers_my_page_path
   end
 
   def after_sign_out_path_for(resource)
@@ -17,6 +18,7 @@ class Public::SessionsController < Devise::SessionsController
 
   # POST /resource/sign_in
   # def create
+  #   # @customer = Customer.new
   #   super
   # end
 
